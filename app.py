8625d9a8bbc1916f6fe6e97eaad184d150f673e6
prateek26 <43009858+prateek26@users.noreply.github.com>
2019-05-25 11:13:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'get rest {} times.\n'.format(count)
+    return 'get restb world {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
